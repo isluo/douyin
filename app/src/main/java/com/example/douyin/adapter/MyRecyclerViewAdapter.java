ffa21ca0509f0565d2ff8ac12ac79be9f82e9417
“isluo <lwt736424400@outlook.com>
2019-06-21 10:50:42 +0800

    luo

--- app/src/main/java/com/example/douyin/adapter/MyRecyclerViewAdapter.java
+++ app/src/main/java/com/example/douyin/adapter/MyRecyclerViewAdapter.java
@@ -1,8 +1,10 @@
 package com.example.douyin.adapter;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -35,13 +37,10 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
 
     private Context context;
     private List<Videos> list_mp4s;
-
     private MyViewHoder myViewHoder;
     public MyRecyclerViewAdapter(Context context, List<Videos> list_mp4s) {
         this.context = context;
         this.list_mp4s = list_mp4s;
-
-
     }
     @NonNull
     @Override
@@ -49,20 +48,42 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
         View view = LayoutInflater.from(context).inflate(R.layout.item_video_page,viewGroup,false);
         return new MyViewHoder(view);
     }
-
+    int b;
     @Override
-    public void onBindViewHolder(@NonNull MyViewHoder myViewHoder, int i) {
+    public void onBindViewHolder(@NonNull final MyViewHoder myViewHoder, int i) {
+        final Drawable topr = ContextCompat.getDrawable(context, R.mipmap.er);
+        topr.setBounds(0, 0, topr.getMinimumWidth(), topr.getMinimumHeight());
+        final Drawable topw = ContextCompat.getDrawable(context, R.mipmap.e);
+        topw.setBounds(0, 0, topw.getMinimumWidth(), topw.getMinimumHeight());
         //myViewHoder.videoView.setVideoURI(Uri.parse("android.resource://"+context.getPackageName()+"/"+ videos[i%2]));
         myViewHoder.img_thumb.setImageUrl(ImgPath.getMp4s(list_mp4s.get(i).getVideopath()));
-        myViewHoder.videoView.setVideoPath(ImgPath.getMp4(list_mp4s.get(i).getVideopath()));
+        //myViewHoder.videoView.setVideoPath(ImgPath.getMp4(list_mp4s.get(i).getVideopath()));
 
         myViewHoder.tv_userID.setText("@"+list_mp4s.get(i).getUserid());
         myViewHoder.tv_ms.setText(list_mp4s.get(i).getVideoIntro());
         myViewHoder.sv_head.setImageUrl(list_mp4s.get(i).getHead());
         myViewHoder.tv_dz.setText(list_mp4s.get(i).getDz()+"");
 
-        this.myViewHoder = myViewHoder;
-        MyVolley.B.selectPlByVideoId.exec(list_mp4s.get(i).getVideoid()).exec(MyRecyclerViewAdapter.this);
+        if(list_mp4s.get(i).isB()){
+            myViewHoder.tv_dz.setCompoundDrawables(null, topr , null, null);
+        }
+         b = i;
+        myViewHoder.tv_dz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(list_mp4s.get(b).isB()){
+                    myViewHoder.tv_dz.setCompoundDrawables(null, topw , null, null);
+                    list_mp4s.get(b).setB(false);
+                }else {
+                    myViewHoder.tv_dz.setCompoundDrawables(null, topr , null, null);
+                    list_mp4s.get(b).setB(true);
+                }
+
+            }
+        });
+
+       /* this.myViewHoder = myViewHoder;
+        MyVolley.B.selectPlByVideoId.exec(list_mp4s.get(i).getVideoid()).exec(MyRecyclerViewAdapter.this);*/
 
         Log.e("AAAAAA"+ImgPath.getMp4(list_mp4s.get(i).getVideopath()),"BBB"+ ImgPath.getMp4(list_mp4s.get(i).getVideopath()));
     }
@@ -106,4 +127,5 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
             tv_userID = itemView.findViewById(R.id.tv_userID);
         }
     }
+
 }
